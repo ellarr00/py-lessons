42ab98a1ebc24d91b5a9f6d081b32b9052e830c6
Ellarr <emmazinny3007@gmail.com>
2023-10-01 23:46:10 -0700

    first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+
+print("Hello world!")
+
+if 5 > 2:
+    print("Five is greater than two!")
+    
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)    
+    
+    
